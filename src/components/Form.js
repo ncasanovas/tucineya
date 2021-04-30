87b95e7191b58fcca4d21c2297fb67051bcf85d2
ncasanovas <nataliasolcasanovas@gmail.com>
2021-04-29 21:44:59 -0300

    Secciones con estilos

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React,  { Fragment } from 'react';
 
 export const Form = () => {
-    return (
-        <div>
-            <h1>Formulario</h1>
-        </div>
+    return (   
+        <Fragment>
+            <form action="/action_page.php">
+                <label for="fname">Email:</label><br />
+                <input type="text" id="fname" name="fname" value="pedro@gmail.com"/><br />
+                <label for="lname">Contraseña:</label><br />
+                <input type="text" id="lname" name="lname"/><br /><br />
+                <input type="submit" value="Ingresar"/><br /><br />
+                <p>¿Todavía no te registraste?</p>
+                <input type="submit" value="Registrarme"/>
+            </form> 
+        </Fragment>
     )
 }
--- src/components/Premiere.js
+++ src/components/Premiere.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 export const Premiere = () => {
     return (
-        <div>
-            <h1>Proximos Estrenos</h1>
+        <div id="premiere">
+            <h1>Próximos Estrenos</h1>
+            <img class="premiere" src="https://i.pinimg.com/originals/f0/6f/f7/f06ff76b83cabc89b7ab475bf36691b6.jpg" alt=""></img> <br /><br />
+            <iframe width="440" height="230" src="https://www.youtube.com/embed/XEMwSdne6UE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <button type="button" class="btn btn-primary">Ver Catálogo de Películas</button>
         </div>
     )
 }
