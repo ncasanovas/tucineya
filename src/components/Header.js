5dcd96c39fb04c0f21c286669e90cd591ff42481
Damian Barrios <barrios.d122@gmail.com>
2021-05-04 20:23:01 -0300

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 export const Header = () => {
-    return (
-        <div>
-            <h1 id="tucineya">TU CINE YA</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1 id="tucineya">TU CINE YA EDITADO</h1>
+    </div>
+  );
+};
