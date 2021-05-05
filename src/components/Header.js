898cbf9bfe72f4555a7c51daf22487862f619931
ncasanovas <nataliasolcasanovas@gmail.com>
2021-05-05 19:48:30 -0300

    estilos y popup

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,20 @@ import React from "react";
 export const Header = () => {
   return (
     <div>
-      <h1 id="tucineya">TU CINE YA</h1>
+        <div id="header" class="row">
+          <div class="col-sm">
+            <img class="icons-home" src="./3d-glasses.png" alt=""/>
+          </div>
+          <div class="col-sm">
+            <h1 id="tucineya">TU CINE YA</h1>
+          </div>
+          <div class="col-sm">
+            <img class="icons-home" src="./clapperboard.png" alt=""/>
+
+          </div>
+        </div>
+      
+
     </div>
   );
 };
