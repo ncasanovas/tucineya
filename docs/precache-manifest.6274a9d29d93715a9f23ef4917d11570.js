4d142e9ddbd8cde5247d3e9b84cadd6cbde96961
ncasanovas <nataliasolcasanovas@gmail.com>
2021-05-15 18:52:28 -0300

    Merge

---
+++ docs/precache-manifest.6274a9d29d93715a9f23ef4917d11570.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22eaaeefd8f32df3552a",
+    "url": "/tucineya/static/css/main.951e07ca.chunk.css"
+  },
+  {
+    "revision": "22eaaeefd8f32df3552a",
+    "url": "/tucineya/static/js/main.761c044e.chunk.js"
+  },
+  {
+    "revision": "ecf6da029721ab3eeeb0",
+    "url": "/tucineya/static/js/runtime~main.ff663aa5.js"
+  },
+  {
+    "revision": "9dfe1407c64256810bf6",
+    "url": "/tucineya/static/js/2.178f24ff.chunk.js"
+  },
+  {
+    "revision": "b2038fc6426e8af19151",
+    "url": "/tucineya/static/js/3.74bca3c3.chunk.js"
+  },
+  {
+    "revision": "7c1e862bf9bb2518274d5dd164276930",
+    "url": "/tucineya/index.html"
+  }
+];
