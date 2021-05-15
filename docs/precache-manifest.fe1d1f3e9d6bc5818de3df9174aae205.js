3bda08002400bb1af9f954e459ed4677b09b3195
ncasanovas <nataliasolcasanovas@gmail.com>
2021-05-15 19:07:47 -0300

    Build

---
+++ docs/precache-manifest.fe1d1f3e9d6bc5818de3df9174aae205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71f839b3e022c0d57c01",
+    "url": "/tucineya/static/css/main.951e07ca.chunk.css"
+  },
+  {
+    "revision": "71f839b3e022c0d57c01",
+    "url": "/tucineya/static/js/main.3fb8d289.chunk.js"
+  },
+  {
+    "revision": "ecf6da029721ab3eeeb0",
+    "url": "/tucineya/static/js/runtime~main.ff663aa5.js"
+  },
+  {
+    "revision": "9485ce25e5ca6177d785",
+    "url": "/tucineya/static/js/2.5893458d.chunk.js"
+  },
+  {
+    "revision": "b2038fc6426e8af19151",
+    "url": "/tucineya/static/js/3.74bca3c3.chunk.js"
+  },
+  {
+    "revision": "98d4d0c31e3bd79226569a00bfb393a9",
+    "url": "/tucineya/index.html"
+  }
+];
