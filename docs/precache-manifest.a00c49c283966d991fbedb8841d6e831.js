323ca0344f0580283fe212029f50832373bc253d
ncasanovas <nataliasolcasanovas@gmail.com>
2021-05-31 19:54:45 -0300

    Build

--- docs/precache-manifest.a00c49c283966d991fbedb8841d6e831.js
+++ docs/precache-manifest.a00c49c283966d991fbedb8841d6e831.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "b29444d075baa5223ec3",
+    "revision": "cec798b71344240fc2d9",
     "url": "/tucineya/static/css/main.f82fc289.chunk.css"
   },
   {
-    "revision": "b29444d075baa5223ec3",
-    "url": "/tucineya/static/js/main.c9403ffe.chunk.js"
+    "revision": "cec798b71344240fc2d9",
+    "url": "/tucineya/static/js/main.0b53b695.chunk.js"
   },
   {
     "revision": "65ae8120df68669f4246",
     "url": "/tucineya/static/js/runtime~main.3e63f42e.js"
   },
   {
-    "revision": "d5f052bd8df4b11d991f",
-    "url": "/tucineya/static/js/2.3e47c52f.chunk.js"
+    "revision": "2956aba7fa1a8e72ac28",
+    "url": "/tucineya/static/js/2.cd812920.chunk.js"
   },
   {
     "revision": "01d4de7054dc422e9efc",
     "url": "/tucineya/static/js/3.136b86f2.chunk.js"
   },
   {
-    "revision": "1909f6bccf84e6fe537f5a6f0ea32f47",
+    "revision": "d70fd2e223856bf40f04f909a1eb9ebf",
     "url": "/tucineya/index.html"
   }
 ];
