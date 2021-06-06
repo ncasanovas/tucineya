5b3ed77c9d51e8b222ecf23dc5815aa157bf818d
ncasanovas <nataliasolcasanovas@gmail.com>
2021-06-06 19:38:22 -0300

    Build

---
+++ docs/precache-manifest.bedecc318ac765754686a39de34c50fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5e59615a1356d06a39a",
+    "url": "/tucineya/static/css/main.7ac0ce91.chunk.css"
+  },
+  {
+    "revision": "e5e59615a1356d06a39a",
+    "url": "/tucineya/static/js/main.fdf83ca2.chunk.js"
+  },
+  {
+    "revision": "65ae8120df68669f4246",
+    "url": "/tucineya/static/js/runtime~main.3e63f42e.js"
+  },
+  {
+    "revision": "272ac90111d30cdb2e2f",
+    "url": "/tucineya/static/js/2.034a2847.chunk.js"
+  },
+  {
+    "revision": "01d4de7054dc422e9efc",
+    "url": "/tucineya/static/js/3.136b86f2.chunk.js"
+  },
+  {
+    "revision": "93aa9ba4b93dbb188bab07423cfee7ed",
+    "url": "/tucineya/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tucineya/precache-manifest.a00c49c283966d991fbedb8841d6e831.js"
+  "/tucineya/precache-manifest.bedecc318ac765754686a39de34c50fd.js"
 );
 
 workbox.clientsClaim();
