a386dbb2c72f98f5a205035c227df7cb6f1a7a97
ncasanovas <nataliasolcasanovas@gmail.com>
2021-06-25 08:00:46 -0300

    Test in Premiere

--- src/tests/components/Premiere.test.js
+++ src/tests/components/Premiere.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {shallow} from 'enzyme';
 import {Premiere} from "../../components/Premiere";
+import { useFetchPremieres } from "../../hooks/useFetchPremieres";
+jest.mock('../../hooks/useFetchPremieres');
 
 describe('Pruebas en <Premiere />', () => {
 
@@ -8,27 +10,50 @@ describe('Pruebas en <Premiere />', () => {
     const title = 'Próximos Estrenos';
 
 
-    test('debe de mostrarse correctamente', () => {
+    test('debe de mostrar <Premiere /> correctamente', () => {
+
+        useFetchPremieres.mockReturnValue({
+            data: [],
+        });
 
         wrapper = shallow(<Premiere />);
+
         expect(wrapper).toMatchSnapshot();
 
     });
 
     test('debe de mostrar correctamente Próximos Estrenos en el h1', () => {
 
+        wrapper = shallow(<Premiere />);
         const titulo = wrapper.find('h1');
         expect(titulo.text().trim()).toBe(title);
     });
 
-   /* test('debe de tener el iframe con la url correspondiente', () => {
-
-        const domain = 'https://www.youtube.com/embed/XEMwSdne6UE';
-        const iframe = wrapper.find('iframe');
-      
-        expect(iframe.prop('src')).toBe(domain);
-       
-    });*/
     
- 
+
+    test('debe de mostrarse la data de las películas de la api', () => {
+
+        const upcoming = [{
+            id: 'ABC',
+            url: 'https://localhost/example.jpg',
+            title: 'Scary Movie'
+        },
+        {
+            id: '123',
+            url: 'https://localhost/example.jpg',
+            title: 'Scary Movie 2'
+        }]
+
+        useFetchPremieres.mockReturnValue({
+            data: upcoming,
+        });
+
+        wrapper = shallow(<Premiere />);
+
+        expect(wrapper).toMatchSnapshot();
+       
+        expect(wrapper.find('PremiereItem').length).toBe(upcoming.length);
+    });
+    
+
 });
