5813962c117cd5b236692e5fe5fee7c002952671
Damian Barrios <barrios.d122@gmail.com>
2021-07-01 19:35:15 -0300

    removed unused components

--- src/tests/components/Suscribirse.test.js
+++ src/tests/components/Suscribirse.test.js
@@ -1,22 +1,24 @@
 import React from "react";
-import {shallow} from 'enzyme';
-import {Suscribirse} from "../../components/Suscribirse";
+import { shallow } from "enzyme";
+import { Suscribirse } from "../../components/Suscribirse";
 
-describe('Pruebas en <Suscribirse />', () => {
+describe("Pruebas en <Suscribirse />", () => {
+  let wrapper;
 
-    let wrapper;
-    
-    test('debe de mostrarse correctamente', () => {
+  test("debe de mostrarse correctamente", () => {
+    wrapper = shallow(<Suscribirse />);
+    expect(wrapper).toMatchSnapshot();
+  });
 
-        wrapper = shallow(<Suscribirse />);
-        expect(wrapper).toMatchSnapshot();
+  test("Debe mostrar el modal", () => {
+    wrapper.find("Button").at(0).simulate("click"); //Boton de eliminar y simulacion de click
 
-    });
+    expect(wrapper.find("Modal").prop("show")).toBe(true); //Si se abre el modal, la prop "show" pasa a true
+  });
 
-    test('debe de mostrar correctamente SUSCRIBETE en el h3', () => {
-
-        const title = "SUSCRIBETE";
-        const titulo = wrapper.find('h3');
-        expect(titulo.text().trim()).toBe(title);
-    });
+  test("debe de mostrar correctamente SUSCRIBETE en el titulo del modal", () => {
+    const title = "Suscribite";
+    const titulo = wrapper.find(".title-sub");
+    expect(titulo.text().trim()).toBe(title);
+  });
 });
