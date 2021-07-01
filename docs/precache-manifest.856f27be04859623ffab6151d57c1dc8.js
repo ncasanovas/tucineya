8d4881474f8e211ac1c7ed0b5116413d24163a2d
Damian Barrios <barrios.d122@gmail.com>
2021-07-01 19:57:21 -0300

    #Entregable_Obligatorio_01

---
+++ docs/precache-manifest.856f27be04859623ffab6151d57c1dc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c53162c7274d7cc676b9",
+    "url": "/tucineya/static/css/main.3cbdd7c7.chunk.css"
+  },
+  {
+    "revision": "c53162c7274d7cc676b9",
+    "url": "/tucineya/static/js/main.e8dfa5ae.chunk.js"
+  },
+  {
+    "revision": "8037dbf3dbe4a7616a9d",
+    "url": "/tucineya/static/js/runtime~main.4ef6bcda.js"
+  },
+  {
+    "revision": "6fcec25ab790ea48de3e",
+    "url": "/tucineya/static/js/2.3a739fbd.chunk.js"
+  },
+  {
+    "revision": "0033426325d49e1216a3",
+    "url": "/tucineya/static/js/3.bcc9bb8e.chunk.js"
+  },
+  {
+    "revision": "10c2949c638eeb79efa22f7e0f1fc90e",
+    "url": "/tucineya/index.html"
+  }
+];
