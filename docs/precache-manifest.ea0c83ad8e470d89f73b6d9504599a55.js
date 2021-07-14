a69503c2e9125f8811b1617f0c6dc59cd73369b5
Damian Barrios <barrios.d122@gmail.com>
2021-07-14 19:49:58 -0300

    build

---
+++ docs/precache-manifest.ea0c83ad8e470d89f73b6d9504599a55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ae338b01402b6f167a4",
+    "url": "/tucineya/static/css/main.3aeebe01.chunk.css"
+  },
+  {
+    "revision": "3ae338b01402b6f167a4",
+    "url": "/tucineya/static/js/main.2526cb1f.chunk.js"
+  },
+  {
+    "revision": "7876fb2118d91df9531c",
+    "url": "/tucineya/static/js/runtime~main.759e1cd9.js"
+  },
+  {
+    "revision": "4f052708204f52ef2c83",
+    "url": "/tucineya/static/css/2.9a2d78d6.chunk.css"
+  },
+  {
+    "revision": "4f052708204f52ef2c83",
+    "url": "/tucineya/static/js/2.70fddb2e.chunk.js"
+  },
+  {
+    "revision": "ab896427e83f94309510",
+    "url": "/tucineya/static/js/3.82bcce39.chunk.js"
+  },
+  {
+    "revision": "f3efd8ace40c0b7202993b28b605b8c4",
+    "url": "/tucineya/index.html"
+  }
+];
