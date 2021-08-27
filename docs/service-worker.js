770ea0b4a9808fc917c5b253c1bdcf212fbddbd1
ncasanovas <nataliasolcasanovas@gmail.com>
2021-08-27 19:25:20 -0300

    Build

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tucineya/precache-manifest.7a5a79cc4746593a129f9e94d7181787.js"
+  "/tucineya/precache-manifest.fed7e7d055ea07365d747eef13d05b27.js"
 );
 
 workbox.clientsClaim();
