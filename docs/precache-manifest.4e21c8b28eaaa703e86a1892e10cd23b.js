073739186e9aabddd0e935f839ea350568c55509
Damian Barrios <barrios.d122@gmail.com>
2021-10-18 18:59:10 -0300

    build

---
+++ docs/precache-manifest.4e21c8b28eaaa703e86a1892e10cd23b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bd1c4afdeacb40b7f34",
+    "url": "/static/css/main.ffadf45e.chunk.css"
+  },
+  {
+    "revision": "9bd1c4afdeacb40b7f34",
+    "url": "/static/js/main.6dfeb3fb.chunk.js"
+  },
+  {
+    "revision": "47637977b7154a4777a7",
+    "url": "/static/js/runtime~main.cdc54bf0.js"
+  },
+  {
+    "revision": "61713d82684bc18d9de3",
+    "url": "/static/css/2.acc7db87.chunk.css"
+  },
+  {
+    "revision": "61713d82684bc18d9de3",
+    "url": "/static/js/2.69bc1e96.chunk.js"
+  },
+  {
+    "revision": "f8a58218b8114de2b6be",
+    "url": "/static/js/3.ff57707e.chunk.js"
+  },
+  {
+    "revision": "67e3903172c92c512e0c24b98a668a54",
+    "url": "/index.html"
+  }
+];
